a089b0a9de3072e90c296a00f0f3405e76075e80
Paul Bagot <bagotpa@gmail.com>
2023-09-12 13:51:52 +0200

    added init game

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+const dimension = 12;
+
 let playButton = document.getElementById('play_button');
-let grid = document.getElementById('grid_body');
+let gridHTML = document.getElementById('grid_body');
+let grid = [[""]];
 let play = false;
 
 playButton.addEventListener('click', () => {
     onclickPlay();
-})
-
-let onclickPlay = () => {
-    play = true;
-    playButton.style.display = 'none';
-}
+});
 
 let snake = () => {
     if(!play) return;
+}
+
+let initgrid = () => {
+    let gridString ='';
+    for(let i = 0; i < dimension; i++) {
+        gridString += '<tr>';
+        for(let j = 0; j < dimension; j++) {
+            gridString += `<td id='case_${i}-${j}'></td>`;
+        }
+        gridString += '</tr>';
+    }
+    gridHTML.innerHTML += gridString;
+}
+
+let onclickPlay = () => {
+    initgrid();
+    play = true;
+    playButton.style.display = 'none';
 }
 
 setInterval(snake, 1000);
