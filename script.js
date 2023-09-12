1ca845eea12f7c72da13ba1eb6e42876de13a404
Paul Bagot <bagotpa@gmail.com>
2023-09-12 20:06:14 +0200

    added the checkSnake function

--- script.js
+++ script.js
@@ -2,18 +2,49 @@ const dimension = 12;
 
 let playButton = document.getElementById('play_button');
 let gridHTML = document.getElementById('grid_body');
-let grid = [[""]];
 let play = false;
 
+let snake = [[6, 6]];
+
 playButton.addEventListener('click', () => {
-    onclickPlay();
+    onClickPlay();
 });
 
-let snake = () => {
+/**
+ * the main function where the game work
+ * @returns 
+ */
+let snakeGame = () => {
     if(!play) return;
 }
+  
 
-let initgrid = () => {
+/**
+ * the function will say if the square is part of the snake
+ * @param {square's line} line 
+ * @param {square's column} column 
+ * @returns 
+ *      - green if the square is part of the body
+ *      - #800080 if the square is the head of the snake
+ *      - white if the square is not part of the body
+ */
+let checkSnake = (line, column) => {
+    for(let i = 0; i < snake.length; i++) {
+        if(snake[i][0] == line && snake[i][1] == column) {
+            if(i == 0)
+                return '#800080';
+            return 'green';
+        }
+    }
+    return 'white';
+}
+
+/**
+ * initialise the game:
+ * create the grid with different colors
+ * depends if it is part of the snake, apple or nothing
+ */
+let initgame = () => {
     let gridString ='';
     for(let i = 0; i < dimension; i++) {
         gridString += '<tr>';
@@ -25,10 +56,14 @@ let initgrid = () => {
     gridHTML.innerHTML += gridString;
 }
 
-let onclickPlay = () => {
-    initgrid();
+/**
+ * call the initilize function 
+ * and makes invisible the play button
+ */
+let onClickPlay = () => {
+    initgame();
     play = true;
     playButton.style.display = 'none';
 }
 
-setInterval(snake, 1000);
+setInterval(snakeGame, 1000);
